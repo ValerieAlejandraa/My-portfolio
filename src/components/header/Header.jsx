265b66c70f991ed39b0161fe98e1950dcc8415bd
Valerie <valerie.perez.alejandra@gmail.com>
2023-08-10 21:42:53 -0700

    added styling to header and intro

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container header__container"></div>
+        <h5>Hello I'm</h5>
+        <h1>Valerie Perez</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+    </header>
+  )
+}
+
+export default Header

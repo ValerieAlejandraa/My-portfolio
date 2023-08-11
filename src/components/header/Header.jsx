6ae08a3b408031a25d7ff468beb5df1731ee1300
Valerie <valerie.perez.alejandra@gmail.com>
2023-08-11 15:05:52 -0700

    added image of myself and links to socials in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.PNG'
 const Header = () => {
   return (
     <header>
-        <div className="container header__container"></div>
+        <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Valerie Perez</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+
+        <div className='me'>
+          <img src={ME} alt="me"/>
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        </div>
     </header>
   )
 }

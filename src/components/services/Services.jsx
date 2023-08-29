8e54bd6bf25d0054c593e0a115dddf517d18623c
Valerie <valerie.perez.alejandra@gmail.com>
2023-08-28 22:18:51 -0700

    added media queries for services content

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import './services.css'
-import {BiCheck} from 'react-icons/bi'
+import React from "react";
+import "./services.css";
+import {BiCheck} from "react-icons/bi";
+
 const Services = () => {
   return (
-    <section id='services'>
+    <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
@@ -12,35 +13,33 @@ const Services = () => {
           <div className="service__head">
             <h3>Web Development</h3>
           </div>
- 
- <ul className='service__list'>
-    <li>
-      <BiCheck className='service__list-icon' />
-      <p>Data Structures and Algorithms</p>
-    </li>
-    <li>
-      <BiCheck className='service__list-icon' />
-      <p>Data Structures and Algorithms</p>
-    </li>
-    <li>
-      <BiCheck className='service__list-icon' />
-      <p>Data Structures and Algorithms</p>
-    </li>
-    <li>
-      <BiCheck className='service__list-icon' />
-      <p>Data Structures and Algorithms</p>
-    </li>
-    <li>
-      <BiCheck className='service__list-icon' />
-      <p>Data Structures and Algorithms</p>
-    </li>
- </ul>
 
-
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Data Structures and Algorithms</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Data Structures and Algorithms</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Data Structures and Algorithms</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Data Structures and Algorithms</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Data Structures and Algorithms</p>
+            </li>
+          </ul>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;

20195c0c8bdb084728e51a354e19ba35e5829591
Valerie <valerie.perez.alejandra@gmail.com>
2023-09-04 15:56:42 -0700

    added project links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/restaurant.jpg'
+import IMG2 from '../../assets/WeLoveMovies.jpg'
+import IMG3 from '../../assets/flashcard.jpg'
+import IMG4 from '../../assets/RecipeTrackerAPP.jpg'
+
+
 
 const Portfolio = () => {
   return (
@@ -10,11 +16,38 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
+            <img src={IMG1} alt="restaurant reservation" />
+          </div>
             <h3>Restaurant Reservations</h3>
             <a href="https://github.com/ValerieAlejandraa/RestaurantReservations" className='btn'>Github</a>
             <a href="https://restaurantreservationfrontend.onrender.com/dashboard?date=2023-09-07" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
 
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="we love movies" />
           </div>
+            <h3>WeLoveMovies</h3>
+            <a href="https://github.com/ValerieAlejandraa/WeLoveMovies" className='btn'>Github</a>
+            <a href="https://welovemoviesfrontend.onrender.com/theaters" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="flashcard app" />
+          </div>
+            <h3>Flashcard App</h3>
+            <a href="https://github.com/ValerieAlejandraa/FlashCardApp" className='btn'>Github</a>
+            <a href="https://flashcard-app-xinh.onrender.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+ 
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="recipe tracker app" />
+          </div>
+            <h3>Recipe Tracker App</h3>
+            <a href="https://github.com/ValerieAlejandraa/RecipeTrackerApp" className='btn'>Github</a>
+            <a href="https://recipetrackerapp.onrender.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
       </div>
     </section>
